6ec6b06369166d96fa9b844d99c61b73380a55d7
veronikargn <86428521+veronikargn@users.noreply.github.com>
2024-07-29 20:00:47 +0700

    Delete tugasakhir2 directory

--- tugasakhir2/start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-if [ "$SCRIPT" == "streamer" ]; then
-  python reddit_mongo.py
-elif [ "$SCRIPT" == "lsa_updater" ]; then
-  python processing.py
-else
-  echo "Invalid SCRIPT variable"
-fi
